fdee5c749a5cfeb705854e9c002c6d5efa50b92d
Klara Nieriker <kn1983@gmail.com>
2012-02-17 11:15:57 +0100

    created an directory for views

---
+++ views/recipe.php
@@ -0,0 +1,17 @@
+<div id="recipe">
+    <a href ="#">MUSIKLÄNK</a>
+    
+    <h1>TITEL</h1>
+    
+    <div id="ingredients">
+        <h2>INGREDIENSER</h2>
+        <ul>
+            <li>likör</li>
+            <li>citron</li>
+            
+        </ul>
+    </div>
+    
+    <div id="descriptions">BESKRIVNING</div>
+    <div id="Instructions">INSTRUKTIONER</div>
+</div>
