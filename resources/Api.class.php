911d64efecd762bea9055728510d2e5669b8216e
Klara Nieriker <kn1983@gmail.com>
2012-02-17 18:53:21 +0100

    added functionality for two levels in the method GET

--- resources/Api.class.php
+++ resources/Api.class.php
@@ -15,27 +15,38 @@
 		     
 		    $resource   = $parts[0];
 		    $id = '';
+		    $idLev2 = '';
+		    $resourceLev2 = '';
+
 		    if(isset($parts[1])){
 		      $id   = $parts[1];
 		    }
+		    if(isset($parts[2])){
+		    	$resource = $parts[2];
+		    	$resourceLev2 = $parts[2];
+		    }
+		    if(isset($parts[3])){
+		    	$idLev2 = $parts[3];
+		    }
+		    
 		    $method   = $_SERVER['REQUEST_METHOD'];
 		     
 		    switch ($method){
-		            case 'GET':
-		                $data = $_GET;
-		                break;
-		            case 'POST':
-		                $data = $_POST;
-		                break;
-		            case 'PUT':
-		                parse_str(file_get_contents('php://input'), $put_vars);
-		                $data = $put_vars;
-		                break;
-		            default: $data = array();
-		        }  
+	            case 'GET':
+	                $data = $_GET;
+	                break;
+	            case 'POST':
+	                $data = $_POST;
+	                break;
+	            case 'PUT':
+	                parse_str(file_get_contents('php://input'), $put_vars);
+	                $data = $put_vars;
+	                break;
+	            default: $data = array();
+	        }  
 		     
 		    require_once($resource.'.resource.php');
-		    $obj = new $resource($method, $id, $data);
+		    $obj = new $resource($method, $id, $idLev2, $data, $resourceLev2);
 		    echo json_encode($obj->data);
 		}
 	}
